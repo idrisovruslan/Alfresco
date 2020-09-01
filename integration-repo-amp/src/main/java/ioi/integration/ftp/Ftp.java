5ed319a737d197b242981805693ff35b1445f978
Руслан <idrisovruslan074@gmail.com>
2020-09-01 20:59:09 +0300

    Возможность чтения XML

--- integration-repo-amp/src/main/java/ioi/integration/ftp/Ftp.java
+++ integration-repo-amp/src/main/java/ioi/integration/ftp/Ftp.java
@@ -101,4 +101,47 @@ public class Ftp {
             throw new Exception(result);
         }
     }
+    /**
+     * Закрытие подключения к FTP
+     * @throws Exception
+     */
+    public void readXmlFromFtp() throws Exception {
+        try {
+            // Создается построитель документа
+            DocumentBuilder documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            // Создается дерево DOM документа из файла
+            Document document = documentBuilder.parse("BookCatalog.xml");
+
+            // Получаем корневой элемент
+            Node root = document.getDocumentElement();
+
+            System.out.println("List of books:");
+            System.out.println();
+            // Просматриваем все подэлементы корневого - т.е. книги
+            NodeList books = root.getChildNodes();
+            for (int i = 0; i < books.getLength(); i++) {
+                Node book = books.item(i);
+                // Если нода не текст, то это книга - заходим внутрь
+                if (book.getNodeType() != Node.TEXT_NODE) {
+                    NodeList bookProps = book.getChildNodes();
+                    for(int j = 0; j < bookProps.getLength(); j++) {
+                        Node bookProp = bookProps.item(j);
+                        // Если нода не текст, то это один из параметров книги - печатаем
+                        if (bookProp.getNodeType() != Node.TEXT_NODE) {
+                            System.out.println(bookProp.getNodeName() + ":" + bookProp.getChildNodes().item(0).getTextContent());
+                        }
+                    }
+                    System.out.println("===========>>>>");
+                }
+            }
+
+        } catch (ParserConfigurationException ex) {
+            ex.printStackTrace(System.out);
+        } catch (SAXException ex) {
+            ex.printStackTrace(System.out);
+        } catch (IOException ex) {
+            ex.printStackTrace(System.out);
+        }
+    }
+    }
 }
