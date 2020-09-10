9943c2daa6259330afad7b8c3a903c3a224a5c6b
Ruslan Idrisov <idrisovruslan074@gmail.com>
2020-09-10 10:54:23 +0300

    Добавил описание всех контейнеров для их создания

--- integration-repo-amp/src/main/java/ioi/integration/dataListServiceWebScript/DescriptionСontainers.java
+++ integration-repo-amp/src/main/java/ioi/integration/dataListServiceWebScript/DescriptionСontainers.java
@@ -184,21 +184,166 @@ public class DescriptionСontainers {
 	public Map<String,Map<String,String>> getDescriptionСontainers() {
     	Map<String,Map<String,String>> descriptionСontainers = new HashMap<String,Map<String,String>>();
     	descriptionСontainers.put("ACKNOVLEDGED_SUPPLIER", this.getDescriptionСontainer(
-    			SITE_SHORT_NAME_DIRECTORIES, 
-    			NAMESPACE_URI_SUPPLIER, 
+    			SITE_SHORT_NAME_DIRECTORIES,
+    			NAMESPACE_URI_SUPPLIER,
     			PREFIX_SUPPLIER,
-    			DATA_LIST_NAME_ACKNOVLEDGED_SUPPLIER, 
-    			CONTENT_MODEL_PROP_TITLE_ACKNOVLEDGED_SUPPLIER, 
-    			CONTENT_MODEL_PROP_DESCRIPTION_ACKNOVLEDGED_SUPPLIER, 
+    			DATA_LIST_NAME_ACKNOVLEDGED_SUPPLIER,
+    			CONTENT_MODEL_PROP_TITLE_ACKNOVLEDGED_SUPPLIER,
+    			CONTENT_MODEL_PROP_DESCRIPTION_ACKNOVLEDGED_SUPPLIER,
     			CONTANER_QNAME_ACKNOVLEDGED_SUPPLIER));
     	descriptionСontainers.put("SUPPLIER_BASE", this.getDescriptionСontainer(
-    			SITE_SHORT_NAME_DIRECTORIES, 
-    			NAMESPACE_URI_SUPPLIER, 
+    			SITE_SHORT_NAME_DIRECTORIES,
+    			NAMESPACE_URI_SUPPLIER,
     			PREFIX_SUPPLIER,
-    			DATA_LIST_NAME_SUPPLIER_BASE, 
-    			CONTENT_MODEL_PROP_TITLE_SUPPLIER_BASE, 
-    			CONTENT_MODEL_PROP_DESCRIPTION_SUPPLIER_BASE, 
+    			DATA_LIST_NAME_SUPPLIER_BASE,
+    			CONTENT_MODEL_PROP_TITLE_SUPPLIER_BASE,
+    			CONTENT_MODEL_PROP_DESCRIPTION_SUPPLIER_BASE,
     			CONTANER_QNAME_SUPPLIER_BASE));
-		return descriptionСontainers;
+        descriptionСontainers.put("PROCUREMENT_DOCUMENT", this.getDescriptionСontainer(
+                SITE_SHORT_NAME,
+                NAMESPACE_URI_PROCUREMENT,
+                PREFIX_PROCUREMENT,
+                DATA_LIST_NAME_PROCUREMENT_DOCUMENT,
+                CONTENT_MODEL_PROP_TITLE_PROCUREMENT_DOCUMENT,
+                CONTENT_MODEL_PROP_DESCRIPTION_PROCUREMENT_DOCUMENT,
+                CONTANER_QNAME_PROCUREMENT_DOCUMENT));
+        descriptionСontainers.put("PROCUREMENT_TYPE", this.getDescriptionСontainer(
+                SITE_SHORT_NAME,
+                NAMESPACE_URI_PROCUREMENT,
+                PREFIX_PROCUREMENT,
+                DATA_LIST_NAME_PROCUREMENT_TYPE,
+                CONTENT_MODEL_PROP_TITLE_PROCUREMENT_TYPE,
+                CONTENT_MODEL_PROP_DESCRIPTION_PROCUREMENT_TYPE,
+                CONTANER_QNAME_PROCUREMENT_TYPE));
+        descriptionСontainers.put("LOT_DOCUMENT", this.getDescriptionСontainer(
+                SITE_SHORT_NAME,
+                NAMESPACE_URI_LOTDOC,
+                PREFIX_LOTDOC,
+                DATA_LIST_NAME_LOT_DOCUMENT,
+                CONTENT_MODEL_PROP_TITLE_LOT_DOCUMENT,
+                CONTENT_MODEL_PROP_DESCRIPTION_LOT_DOCUMENT,
+                CONTANER_QNAME_LOT_DOCUMENT));
+        descriptionСontainers.put("LOT_POSITION", this.getDescriptionСontainer(
+                SITE_SHORT_NAME,
+                NAMESPACE_URI_LOTDOC,
+                PREFIX_LOTDOC,
+                DATA_LIST_NAME_LOT_POSITION,
+                CONTENT_MODEL_PROP_TITLE_LOT_POSITION,
+                CONTENT_MODEL_PROP_DESCRIPTION_LOT_POSITION,
+                CONTANER_QNAME_LOT_POSITION));
+        descriptionСontainers.put("POSITION", this.getDescriptionСontainer(
+                SITE_SHORT_NAME,
+                NAMESPACE_URI_LOTDOC,
+                PREFIX_LOTDOC,
+                DATA_LIST_NAME_POSITION,
+                CONTENT_MODEL_PROP_TITLE_POSITION,
+                CONTENT_MODEL_PROP_DESCRIPTION_POSITION,
+                CONTANER_QNAME_POSITION));
+        descriptionСontainers.put("QUALIFIER_TYPE", this.getDescriptionСontainer(
+                SITE_SHORT_NAME,
+                NAMESPACE_URI_LOTDOC,
+                PREFIX_LOTDOC,
+                DATA_LIST_NAME_QUALIFIER_TYPE,
+                CONTENT_MODEL_PROP_TITLE_QUALIFIER_TYPE,
+                CONTENT_MODEL_PROP_DESCRIPTION_QUALIFIER_TYPE,
+                CONTANER_QNAME_QUALIFIER_TYPE));
+        descriptionСontainers.put("DELIVERY_GRAF", this.getDescriptionСontainer(
+                SITE_SHORT_NAME,
+                NAMESPACE_URI_LOTDOC,
+                PREFIX_LOTDOC,
+                DATA_LIST_NAME_DELIVERY_GRAF,
+                CONTENT_MODEL_PROP_TITLE_DELIVERY_GRAF,
+                CONTENT_MODEL_PROP_DESCRIPTION_DELIVERY_GRAF,
+                CONTANER_QNAME_DELIVERY_GRAF));
+        descriptionСontainers.put("DELIVERY_SELL_OPT", this.getDescriptionСontainer(
+                SITE_SHORT_NAME,
+                NAMESPACE_URI_LOTDOC,
+                PREFIX_LOTDOC,
+                DATA_LIST_NAME_DELIVERY_SELL_OPT,
+                CONTENT_MODEL_PROP_TITLE_DELIVERY_SELL_OPT,
+                CONTENT_MODEL_PROP_DESCRIPTION_DELIVERY_SELL_OPT,
+                CONTANER_QNAME_DELIVERY_SELL_OPT));
+        descriptionСontainers.put("SALE_GRAF", this.getDescriptionСontainer(
+                SITE_SHORT_NAME,
+                NAMESPACE_URI_LOTDOC,
+                PREFIX_LOTDOC,
+                DATA_LIST_NAME_SALE_GRAF,
+                CONTENT_MODEL_PROP_TITLE_SALE_GRAF,
+                CONTENT_MODEL_PROP_DESCRIPTION_SALE_GRAF,
+                CONTANER_QNAME_SALE_GRAF));
+        descriptionСontainers.put("SALE_SELL_OPT", this.getDescriptionСontainer(
+                SITE_SHORT_NAME,
+                NAMESPACE_URI_LOTDOC,
+                PREFIX_LOTDOC,
+                DATA_LIST_NAME_SALE_SELL_OPT,
+                CONTENT_MODEL_PROP_TITLE_SALE_SELL_OPT,
+                CONTENT_MODEL_PROP_DESCRIPTION_SALE_SELL_OPT,
+                CONTANER_QNAME_SALE_SELL_OPT));
+        descriptionСontainers.put("MANUFACTURER", this.getDescriptionСontainer(
+                SITE_SHORT_NAME_DIRECTORIES,
+                NAMESPACE_URI_DIRECTORY,
+                PREFIX_DIRECTORY,
+                DATA_LIST_NAME_MANUFACTURER,
+                CONTENT_MODEL_PROP_TITLE_MANUFACTURER,
+                CONTENT_MODEL_PROP_DESCRIPTION_MANUFACTURER,
+                CONTANER_QNAME_MANUFACTURER));
+        descriptionСontainers.put("SAFETY_CLASS", this.getDescriptionСontainer(
+                SITE_SHORT_NAME_DIRECTORIES,
+                NAMESPACE_URI_DIRECTORY,
+                PREFIX_DIRECTORY,
+                DATA_LIST_NAME_SAFETY_CLASS,
+                CONTENT_MODEL_PROP_TITLE_SAFETY_CLASS,
+                CONTENT_MODEL_PROP_DESCRIPTION_SAFETY_CLASS,
+                CONTANER_QNAME_SAFETY_CLASS));
+        descriptionСontainers.put("MATERIALS_LIST", this.getDescriptionСontainer(
+                SITE_SHORT_NAME_DIRECTORIES,
+                NAMESPACE_URI_DIRECTORY,
+                PREFIX_DIRECTORY,
+                DATA_LIST_NAME_MATERIALS_LIST,
+                CONTENT_MODEL_PROP_TITLE_MATERIALS_LIST,
+                CONTENT_MODEL_PROP_DESCRIPTION_MATERIALS_LIST,
+                CONTANER_QNAME_MATERIALS_LIST));
+        descriptionСontainers.put("PAYMENT_CONDITIONS", this.getDescriptionСontainer(
+                SITE_SHORT_NAME_DIRECTORIES,
+                NAMESPACE_URI_DIRECTORY,
+                PREFIX_DIRECTORY,
+                DATA_LIST_NAME_PAYMENT_CONDITIONS,
+                CONTENT_MODEL_PROP_TITLE_PAYMENT_CONDITIONS,
+                CONTENT_MODEL_PROP_DESCRIPTION_PAYMENT_CONDITIONS,
+                CONTANER_QNAME_PAYMENT_CONDITIONS));
+        descriptionСontainers.put("PRICE_VALIDITY_IN_TKP", this.getDescriptionСontainer(
+                SITE_SHORT_NAME_DIRECTORIES,
+                NAMESPACE_URI_DIRECTORY,
+                PREFIX_DIRECTORY,
+                DATA_LIST_NAME_PRICE_VALIDITY_IN_TKP,
+                CONTENT_MODEL_PROP_TITLE_PRICE_VALIDITY_IN_TKP,
+                CONTENT_MODEL_PROP_DESCRIPTION_PRICE_VALIDITY_IN_TKP,
+                CONTANER_QNAME_PRICE_VALIDITY_IN_TKP));
+        descriptionСontainers.put("ICI_SEARCH_RESULTS", this.getDescriptionСontainer(
+                SITE_SHORT_NAME,
+                NAMESPACE_URI_PRICEINF,
+                PREFIX_PRICEINF,
+                DATA_LIST_NAME_ICI_SEARCH_RESULTS,
+                CONTENT_MODEL_PROP_TITLE_ICI_SEARCH_RESULTS,
+                CONTENT_MODEL_PROP_DESCRIPTION_ICI_SEARCH_RESULTS,
+                CONTANER_QNAME_ICI_SEARCH_RESULTS));
+        descriptionСontainers.put("CALCULATION_AND_JUSTIFICATION_NMC", this.getDescriptionСontainer(
+                SITE_SHORT_NAME,
+                NAMESPACE_URI_PRICEINF,
+                PREFIX_PRICEINF,
+                DATA_LIST_NAME_CALCULATION_AND_JUSTIFICATION_NMC,
+                CONTENT_MODEL_PROP_TITLE_CALCULATION_AND_JUSTIFICATION_NMC,
+                CONTENT_MODEL_PROP_DESCRIPTION_CALCULATION_AND_JUSTIFICATION_NMC,
+                CONTANER_QNAME_CALCULATION_AND_JUSTIFICATION_NMC));
+        descriptionСontainers.put("ATTRIBUTIVE_STRUCTURE_OF_THE_AZTKP_FACILITY", this.getDescriptionСontainer(
+                SITE_SHORT_NAME,
+                NAMESPACE_URI_AZTKP,
+                PREFIX_AZTKP,
+                DATA_LIST_NAME_ATTRIBUTIVE_STRUCTURE_OF_THE_AZTKP_FACILITY,
+                CONTENT_MODEL_PROP_TITLE_ATTRIBUTIVE_STRUCTURE_OF_THE_AZTKP_FACILITY,
+                CONTENT_MODEL_PROP_DESCRIPTION_ATTRIBUTIVE_STRUCTURE_OF_THE_AZTKP_FACILITY,
+                CONTANER_QNAME_ATTRIBUTIVE_STRUCTURE_OF_THE_AZTKP_FACILITY));
+
+        return descriptionСontainers;
 	}
 }
